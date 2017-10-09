44c75649316fffb0c4ef37851762c47a444a913d
keerthanaselvam17 <32628970+keerthanaselvam17@users.noreply.github.com>
2017-10-09 11:34:27 +0530

    Add files via upload

---
+++ read.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int s,a;
+    printf("\n enter the number");
+    scanf("%d",$s);
+    if(a==%2)
+}
+printf("\n the number is even",$s);
+else
+    printf("\n the number is odd",$s);
+return 0;
+}
